13b7cba1bfd7c6f213f245f7dcea3ddab1f0d08d
Michael Munoz <mun21026@byui.edu>
2023-04-26 23:52:24 -0600

    Prep 3 Assignment Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.WriteLine("The rules are simple, guess the magic number!");
+        Console.WriteLine("");
+        string play = "Y";
+        int guesses = 0;
+        int guess = 0;
+        string input = "";
+        int number = randomGenerator.Next(1,100);
+        do {
+            Console.WriteLine("What is your guess?");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            guesses++;
+            if (guess > number) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number) {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number) {
+                Console.WriteLine("Congrats! You guessed the magic number!");
+                Console.WriteLine($"Number of guesses: {guesses}.");
+                Console.WriteLine("Would you like to play again? (Enter 'Y' or 'N'): ");
+                play = Console.ReadLine();
+                number = randomGenerator.Next(1,100);
+                guesses = 0;
+            }
+        }  while (play == "Y");
+        Console.WriteLine("Thanks for playing!");
     }
 }
